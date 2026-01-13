e17619752d582841f205cfd7a66d18142f639a9e
madobeno <jittersquatstore@gmail.com>
2026-01-13 22:23:08 +0900

    Fix paths for GitHub Pages PWA

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('sakuraame-v1').then(cache =>
+      cache.addAll([
+        './',
+        './index.html'
+      ])
+    )
+  );
+});
